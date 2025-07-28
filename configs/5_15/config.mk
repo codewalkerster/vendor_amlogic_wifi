92ea995af9a6aa686a66616ddb2b8500465e0cca
jiabin.chen <jiabin.chen@amlogic.com>
2025-07-28 16:09:28 +0800

    wifi: w2 config CONFIG_MAGIC_PACKET_EN [1/1]
    PD#SWPL-213620
    
    Problem:
    w2 wifi not support magic power on
    
    Solution:
    config CONFIG_MAGIC_PACKET_EN
    
    Verify:
    calla
    
    Change-Id: I0fc94d62c60125f34b6eedf12ff75e8f5d755224
    Signed-off-by: jiabin.chen <jiabin.chen@amlogic.com>

--- configs/5_15/config.mk
+++ configs/5_15/config.mk
@@ -89,7 +89,7 @@ w2_modules ?= w2
 w2_src_path ?= $(DRIVER_DIR)/amlogic/w2
 w2_copy_path ?=
 w2_build_path ?= aml_drv
-w2_args ?= CONFIG_ANDROID_GKI=y CONFIG_AML_ANDROID=14 CONFIG_MDNS_OFFLOAD_FEATRUE=n
+w2_args ?= CONFIG_ANDROID_GKI=y CONFIG_AML_ANDROID=14 CONFIG_MDNS_OFFLOAD_FEATRUE=n CONFIG_MAGIC_PACKET_EN=y CONFIG_GOOGLE_CAST_EN=y
 ifeq ($(w2_build),true)
 WIFI_BUILT_MODULES += $(w2_modules)
 endif
